2ca980c7eb18d8a63d4e7ea0b28f191395e298e7
HughesSeanKyle <khughessean@yahoo.com>
2020-11-27 08:12:09 +0200

    V80 - Test Project Updates

--- compile.js
+++ compile.js
@@ -4,12 +4,12 @@ const path = require('path');
 const fs = require('fs');
 const solc = require('solc'); //require solidity compiler. 
 
-const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol');
+const lotteryPath = path.resolve(__dirname, 'contracts', 'Lottery.sol');
 //read RAW source code from contract
-const source = fs.readFileSync(inboxPath, 'utf8');
+const source = fs.readFileSync(lotteryPath, 'utf8');
 
 
-module.exports =  solc.compile(source, 1).contracts[':Inbox']; //args 1: What to compile, 2: How many different contracts to compile.
+module.exports =  solc.compile(source, 1).contracts[':Lottery']; //args 1: What to compile, 2: How many different contracts to compile.
 
 
 
